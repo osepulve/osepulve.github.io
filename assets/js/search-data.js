28a3829a4e160b780450ca114c14cad5ff7db10d
osepulve <oscarsepulveda32@gmail.com>
2025-12-23 00:44:40 +0000

    Deploying to gh-pages from @ osepulve/osepulve.github.io@49451ec1152ccd22271b242b7218aa0bd10df11e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-bookshelf",
-          title: "bookshelf",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/books/";
-          },
-        },{id: "nav-blog",
+  },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
