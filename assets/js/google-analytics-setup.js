9ad2713fffbd684861af2b1f468ca656a4ef0f19
osepulve <oscarsepulveda32@gmail.com>
2026-02-18 12:17:42 -0800

    Included Google analytics capability
    1) Added Google analytics G-XXXXXXXXXX and modified enable_google_analytics: true. 2) Added file google-analytics-setup.js. to assets/js/

---
+++ assets/js/google-analytics-setup.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){ window.dataLayer.push(arguments); }
+gtag('js', new Date());
+
+// Replace G-XXXXXXXXXX with your actual Measurement ID 
+// or ensure it matches what is in your _config.yml
+gtag('config', 'G-Y1XGMNDNZ5);
